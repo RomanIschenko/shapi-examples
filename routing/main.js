0df9f93a52a5bb6af31e96064f6d7d7de99c51fd
RomanIschenko <formypsnspb@gmail.com>
2020-08-11 14:55:05 +0300

    routing update

--- routing/main.js
+++ routing/main.js
@@ -1,6 +1,6 @@
 const Shapi = require(`shapi`)
 const { logger } = require('shapi/lib/std/mw/logger')
-const { html, json } = require('shapi/std/replies')
+const { html, json, redirect } = require('shapi/std/replies')
 const { readFileSync } = require(`fs`)
 
 let users = JSON.parse(readFileSync('users.json').toString())
@@ -9,44 +9,58 @@ let api = new Shapi
 
 // api.use(logger('[api]'))
 
-let usersEndpoint = api.with('users')
+const endpoint = api.with('users')
 
-usersEndpoint.with('get')
-    .do(() => json(users))
+endpoint.with('/get')
+    .on('/')
+        .do(()=>json(users))
     .on('{id: int}')
-        .do(id => {
-            return json({ user: users[id] })
-        })
-    .on('{name: string}')
-        .do(name => {
-            let tmp = []
-            for (let id in users) {
+        .do(id =>{
+            return json({user: users[id]})
+        } )
+    .on('{data: string}')
+        .do(data => {
+            data = data.toLowerCase()
+            let result = []
+            for(let id in users) {
                 let user = users[id]
-                if (user.name == name)
-                    tmp.push(user)
+                if(user.name.toLowerCase().includes(data) || user.surname.toLowerCase().includes(data)) {
+                    result.push(user)
+                }
             }
-            return json(tmp)
-        });
-
-usersEndpoint.with('create')
-    .on('{name: string}/{surname: string}')
-    .do((name, surname) => {
-        let ids = Object.keys(users)
-        let lastID = parseInt(ids[ids.length - 1])
-        let id = lastID + 1
-        users[id] = { name, surname, id }
-        return html(`
-                    <h1>${name}</h1>
-                    <h3>${surname}</h3>
-                    `)
-    })
-
-usersEndpoint.with('get')
-    .with('{somestuff}')
-    .with('{otherstuff}')
-    .with('{otherotherstuff}')
-        .on('{id: int}').do((somestuff, otherstuff, otherotherstuff, id) => {
-            return json({ somestuff, otherstuff, otherotherstuff, id })
+            return json(result)
         })
+    .on('{name:string}/{surname: string}')
+        .do((name, surname) => {
+            let result = []
+            for(let id in users) {
+                let user = users[id]
+                if(user.surname == surname && user.name == name)
+                    result.push(user)
+            }
+            return json(result)
+        })
+
+endpoint.with('/create')
+    .on('{name: string}/{surname: string}')
+        .do((name, surname) => {
+            const ids = Object.keys(users)
+            const id = parseInt(ids[ids.length-1]) + 1
+            
+            users[id] = {
+                id, name, surname
+            }
+
+            return redirect(`/users/get/${id}`)
+        })
+
+api.on('timeout/{ms: int}')
+    .do(ms => {
+        return new Promise(
+            ok => setTimeout(
+                    ()=>ok(json({})), ms
+                )
+            )
+    })
 
 api.run()
